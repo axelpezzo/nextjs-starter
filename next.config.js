bf0da60fc098a0828059bb96cf958244d78becd0
Alex Pezzini <alex.pezzini@intesys.it>
2020-11-02 17:30:07 +0100

    fix loader vercel

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  images: {
+    deviceSizes: [320, 768, 1200],
+    iconSizes: [16, 32, 64],
+    domains: [],
+    path: '/_next/image',
+    loader: 'default',
+    images: {
+      loader: 'vercel',
+    },
+  },
+}
