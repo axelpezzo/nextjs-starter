b42d8b68ffcb2076779962efc0025f5365921b02
Alex Pezzini <alex.pezzini@intesys.it>
2020-11-03 12:00:45 +0100

    add content

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,10 @@ export default function Home ({ allPostsData }) {
         <title>Homepage</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Sono appassionato di Web e Informatica, passione portata avanti fin da quando ero bambino. Per questo motivo mi sono diplomato come Perito Tecnico Informatico all’ITIS di Mantova. Nonostante l’interruzione degli studi per dedicarmi a quest’attività a tempo pieno, mi ritaglio ogni settimana del tempo dedito alla formazione e allo studio.</p>
+        <p>Lavoro da anni come sviluppatore Full-Stack, con tecnologie Web come Drupal e AWS, partecipando ogni anno ad eventi o corsi di formazione.</p>
+        <p>Negli ultimi anni mi sono avvicinato molto al mondo dello sviluppo di Applicazioni Web e Mobile Ibrido, studiando e sperimentando tecnologie fra le quali: Node.js, React (+Redux) e Angular (Ionic).</p>
+        <p>Sono curioso di natura, amo le sfide e il mettermi continuamente in gioco.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
