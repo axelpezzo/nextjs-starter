3da217f75005e7c320bf538c4fb0c14cf6dd816d
Alex Pezzini <alex.pezzini@intesys.it>
2020-11-03 11:28:25 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
-module.exports = {
-  images: {
-    deviceSizes: [320, 768, 1200],
-    iconSizes: [16, 32, 64],
-    domains: [],
-    path: '/_next/image',
-    loader: 'default',
-  },
-}
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
 
-const withOptimizedImages = require('next-optimized-images');
- 
-module.exports = withOptimizedImages({
-  /* config for next-optimized-images */
- 
-  // your config for other plugins or the general next.js here...
-});
+module.exports = withPlugins([
+  optimizedImages,
+]);
